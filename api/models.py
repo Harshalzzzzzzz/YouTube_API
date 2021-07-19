9b20a399dd0fd7486c78f83c6f450d3821233ff3
Harshal.S <harshal2015@gmail.com>
2021-07-19 20:37:30 +0530

    Django Application, Youtube API

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Videos(models.Model):
+    video_id = models.CharField(max_length=500, unique=True)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=200)
+    published_at = models.DateTimeField()
+    thumbnail_url = models.URLField()
+    channel_title = models.CharField(max_length=150)
+    channel_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
