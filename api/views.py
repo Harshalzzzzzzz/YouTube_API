b9f67bb42c4e5213062db56bdcdcfb0cf8bb9a0c
Harshal.S <harshal2015@gmail.com>
2021-07-19 21:28:59 +0530

    Changed urls, views. Added Pagination for the interface.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+# Rest FrameWork
+from rest_framework import generics
+from rest_framework.pagination import CursorPagination
 
-# Create your views here.
+from ._serialize import VideoSerializer
+from .models import *
+
+
+class ResultsPagination(CursorPagination):
+    page_size = 25
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class VideoItems(generics.ListAPIView):
+    search_fields = ['title', 'description']
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend, filters.OrderingFilter)
+    filterset_fields = ['channel_id', 'channel_title']
+    ordering = ('-published_at')
+    queryset = Videos.objects.all()
+    serializer_class = VideoSerializer
+    pagination_class = ResultsPagination
+
--- youtube/settings.py
+++ youtube/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'rest_framework',
     'api'
 ]
 
@@ -121,3 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'PAGE_SIZE': 10
+}
